d40a72d73714a22852354b8c87c5301a193b6ab0
Outlandishh <63488363+Outlandishh@users.noreply.github.com>
2021-09-02 14:17:51 +1000

    Added commenting & help.html
    Python client is complete, with working help.html file

--- client.py
+++ client.py
@@ -1,39 +1,48 @@
+# Import the libraries that are needed for the program
 import kivy
 import socket
 import sys
 import webbrowser
 
-
+# The current Kivy version is 2.0.0
 kivy.require('2.0.0')  # current kivy version
 
-
+# Specific modules used from the libraries imported above
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import StringProperty
 from kivy.graphics import *
 
-
+# Create the GUI, assign a size for the display
 class FloatLayout(FloatLayout):
     Window.size = (360, 200)
     status_text = StringProperty()
     ip_address = StringProperty()
 
+    # Function for finding and connecting to the server
     def connect_to_server(self, instance, value):
+        # Default position is 'Not Connected'
         self.status_text = 'Not Connected'
 
-        # https://realpython.com/python-sockets/#echo-client
-        HOST = self.ip.text  # the server's hostname or IP address
-        PORT = 6789  # port used by the server
+        # Define the host/IP address and port numbers
+        HOST = self.ip.text  # The server's hostname or IP address
+        PORT = 6789  # Port used by the server
+
+        # Create the socket and attempt to connect to the server
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.settimeout(0.1)
             s.connect((HOST, PORT))
             self.status_text = 'Connection Test Successful \nDrag and Drop Files to Send'
 
-            # Drag and drop functionality:
+            # Add drag and drop functionality:
             Window.bind(on_dropfile=self._on_file_drop)
 
+            # Sending and receiving data
+            # If the client detects an empty input value, display
+            # that nothing was sent. When the input values are
+            # correctly sent, output how many characters were sent
             if value != '':
                 sent = str(s.send(value.encode()))
                 if sent == 0:
@@ -44,33 +53,45 @@ class FloatLayout(FloatLayout):
                     self.status_text = (sent + ' Characters Sent Successfully')
                 print(s)
 
+            # Shut the socket down and close the connection
             s.shutdown(socket.SHUT_RDWR)
             s.close()
             print(s)
 
+        # Exception handling for timeouts or no connectivity
         except socket.timeout as e:
-            self.status_text = ('Connection Test Failure: \n' + str(e))
+            self.status_text = ('Connection Timeout Failure: \n' + str(e))
             print(e)
         except socket.error as e:
-            self.status_text = ('Connection Test Failure: \n' + str(e))
+            self.status_text = ('Connection Not Found: \n' + str(e))
             print(e)
         return
 
+    # Functionality for adding documents to the server.
+    # When a file is dropped on the window, read the file
+    # and send the data to the connect_to_server function
+    # who in turn forwards it to the server
     def _on_file_drop(self, window, file_path):
         print(file_path)
-        data = open(file_path, 'r').read()  # reading file as string
+        data = open(file_path, 'r').read()  # Read the file as string
         self.connect_to_server('', data)
         return
 
+    # Create a help menu button to open the 'help.html' file hosted locally
+    # on my machine in the /var/www/html folder.
     def help(self):
-        webbrowser.open('help.html', new=2, autoraise=True)
+        webbrowser.open('http://192.168.10.112/help.html', new=2, autoraise=True)
 
+    # Create the exit function
     def exit(self):
         sys.exit()
 
+    # Initialise the GUI
     def __init__(self, **kwargs):
         super(FloatLayout, self).__init__(**kwargs)
 
+        # Add 2D graphics to make the program just
+        # that little bit prettier... kinda
         with self.canvas:
             # Draw a red rectangle:
             Color(0, 0, 1, 0.5, mode="rgba")
@@ -80,6 +101,7 @@ class FloatLayout(FloatLayout):
             Line(points=(320, 5, 335, 45, 350, 5, 315, 30, 355, 30, 320, 5))
 
 
+# Build the application
 class MyApp(App):
     def build(self):
         self.title = 'Python client'
